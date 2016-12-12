4bc9465380a92adf470b8ecc0e30bc8efe9fbff6
Bernix01 <gbernal096@gmail.com>
2016-12-12 09:23:37 -0500

    seed and fix

--- models/lugarSchema.js
+++ models/lugarSchema.js
@@ -1,15 +1,33 @@
-var LugarSchema= function(){
-  var mongoose= require('../db').mongoose;
-  var schema={
-    nombre:{type:String,required:true},
-    descripcion:{type:String,required:false},
-    etiquetas:{type:[String],required:false},
-    lat:{type: Number ,required:true},
-    lng:{type: Number,required:true},
-    imagen:{type:String,required:false}
-  };
-  var lugarSchema=mongoose.Schema(schema);
-  var Lugar=mongoose.model('Lugar',lugarSchema);
-  return Lugar;
+var LugarSchema = function() {
+    var mongoose = require('../db').mongoose;
+    var schema = {
+        nombre: {
+            type: String,
+            required: true
+        },
+        descripcion: {
+            type: String,
+            required: false
+        },
+        etiquetas: {
+            type: [String],
+            required: false
+        },
+        lat: {
+            type: Number,
+            required: true
+        },
+        lng: {
+            type: Number,
+            required: true
+        },
+        imagen: {
+            type: String,
+            required: false
+        }
+    };
+    var lugarSchema = mongoose.Schema(schema);
+    var Lugar = mongoose.model('Lugar', lugarSchema);
+    return Lugar;
 };
 module.exports = LugarSchema;
--- routes/rutasLugar.js
+++ routes/rutasLugar.js
@@ -1,13 +1,13 @@
 module.exports = function(app) {
-	var usuarioController= require('../controllers/usuarioController');
-	var lugarController=require('../controllers/lugarController');
-	app.get('/', function(req, res, next) {
-		return res.send("WELCOME TO REST API");
-	});
-	app.get('/otraRuta',function(req,res,next){
-		return res.send("another page hehe");
-	});
+    var usuarioController = require('../controllers/usuarioController');
+    var lugarController = require('../controllers/lugarController');
+    app.get('/', function(req, res, next) {
+        return res.send("WELCOME TO REST API");
+    });
+    app.get('/otraRuta', function(req, res, next) {
+        return res.send("another page hehe");
+    });
 
-	app.post('/getUsuario',usuarioController.createUsuario); //Create Student API
-	app.get('/getLugar',lugarController.getLugares); //Gets ALL students details API
+    app.post('/getUsuario', usuarioController.createUsuario); //Create Student API
+    app.get('/getLugar', lugarController.getLugares); //Gets ALL students details API
 };
